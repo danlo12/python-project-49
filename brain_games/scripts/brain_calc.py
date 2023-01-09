c42a19b08a2cf7347172159bcce5b9a32b789f57
danlo <danlo12@mail.ru>
2023-01-10 00:14:39 +0300

    ex 7

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from brain_games.game_engine import run
-from brain_games.games import calculate_game
+from brain_games.games import calculate
 
 
 def main():
-    run(calculate_game)
+    run(calculate)
 
 
 if __name__ == '__main__':
